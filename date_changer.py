8df939830b1dc278db643edd0e05fd72ccb38baa
Vladislav Nikitin <nikitinvdmail@yandex.ru>
2024-12-24 03:55:51 +0300

    Add files via upload

---
+++ date_changer.py
@@ -0,0 +1,14 @@
+username = ("Владислав")
+title = ("Планы")
+content = ("То, что нужно сделать")
+status = ("Активна")
+created_date = ("18-12-2024")
+issue_date = ("31-12-2024")
+temp_created_date = created_date[:5]
+temp_issue_date = issue_date[:5]
+print("Имя пользователя:", username)
+print("Заголовок заметки:", title)
+print("Описание заметки:", content)
+print("Статус заметки:", status)
+print("Дата создания заметки:", temp_created_date)
+print("Дата истечения заметки:", temp_issue_date)
